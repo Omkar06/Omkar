55f7eb2bf7d592ea167fa5328785a084fc8a1a8d
Omkar <omkar@gmail.com>
2018-10-05 23:49:45 +0530

    first commit

---
+++ acm.php
@@ -0,0 +1,240 @@
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+
+
+
+<body>
+
+	body {
+	    background-image: url("https://upload.wikimedia.org/wikipedia/commons/thumb/0/07/Blue_%286100521867%29.jpg/1200px-Blue_%286100521867%29.jpg");
+	}
+
+</style>
+</head>
+<body>
+<head>
+	<title>sample</title>
+	<link rel="stylesheet" href="style.css">
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+</head>
+
+<head>
+<style>
+
+
+
+<body>
+
+	body {
+	    background-image: url("https://upload.wikimedia.org/wikipedia/commons/thumb/0/07/Blue_%286100521867%29.jpg/1200px-Blue_%286100521867%29.jpg");
+	}
+
+</style>
+</head>
+<body>
+
+
+
+
+<!-- Image and text -->
+<nav class="navbar navbar-light bg-light">
+  <a class="navbar-brand" href="#">
+    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/07/Blue_%286100521867%29.jpg/1200px-Blue_%286100521867%29.jpg" width="30" height="30" class="d-inline-block align-center" alt="">
+    Band
+  </a>
+</nav>
+
+
+
+
+
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+  <a class="navbar-brand" href="#">News</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+     <div class="collapse navbar-collapse" id="navbarNavDropdown">
+
+    <ul class="navbar-nav">
+		      <li class="nav-item active">
+        <a class="nav-link" href="#">Tour<span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Shop</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Music</a>
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          More
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+          <a class="dropdown-item" href="#">Archieves</a>
+          <a class="dropdown-item" href="#">About</a>
+          <a class="dropdown-item" href="#">Contact</a>
+        </div>
+      </li>
+    </ul>
+  </div>
+</nav>
+
+
+
+
+
+<nav class="navbar navbar-light bg-light">
+  <a class="navbar-brand">City and get dates</a>
+  <form class="form-inline">
+    <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+
+  </form>
+</nav>
+
+
+
+
+
+
+
+<div class="shadow p-3 mb-5 bg-white rounded"><strong>EAT SLEEP MIX REPEAT !!!</strong></div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<!-- A grey horizontal navbar that becomes vertical on small screens -->
+<nav class="navbar navbar-expand-sm bg-light">
+
+  <!-- Links -->
+  <ul class="navbar-nav">
+    <li class="nav-item">
+      <a class="nav-link" href="#"></a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="#"></a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="#"></a>
+    </li>
+  </ul>
+
+</nav>
+
+<section class="w3-container w3-center w3-content" style="max-width:600px">
+  <h2 class="w3-wide">THE BAND</h2>
+  <p class="w3-opacity"><i>We love music</i></p>
+  <p class="w3-justify">We have created a fictional band website.A rock band or pop band is a small musical ensemble which performs rock music, pop music or a related genre. The four-piece band is the most common configuration in rock and pop music. Before the development of the electronic keyboard, the configuration was typically two guitarists (a lead guitarist and a rhythm guitarist, with one of them singing lead vocals), a bassist, and a drummer (e.g. the Beatles, KISS, Metallica). Another common formation is a vocalist who does not play an instrument, electric guitarist, bass guitarist, and a drummer (e.g. the Who, the Monkees, Led Zeppelin, Queen, and U2). Instrumentally, these bands can be considered as trios.</p>
+</section>
+
+
+
+<nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+  <!-- Brand/logo -->
+  <a class="navbar-brand" href="#">FOLLOW   US</a>
+
+  <!-- Links -->
+  <ul class="navbar-nav">
+    <li class="nav-item">
+      <a class="nav-link" href="#">facebook</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="#">instagram</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="#">youtube</a>
+    </li>
+	 <li class="nav-item">
+      <a class="nav-link" href="#">twitter</a>
+    </li>
+  </ul>
+</nav>
+
+
+
+
+
+<!-- Just an image -->
+<nav class="navbar navbar-light bg-light">
+  <a class="navbar-brand" href="#">
+    
+	<img src="https://www.google.co.in/search?rlz=1C1CHBF_enIN808IN808&biw=1366&bih=657&tbm=isch&sa=1&ei=nJy3W-uZA4LVvgSY-rL4Bg&q=bands&oq=bands&gs_l=img.3..0i67k1j0l2j0i67k1l3j0l2j0i67k1l2.243097.245866.0.246040.21.9.0.0.0.0.196.822.0j6.6.0....0...1c.1.64.img..15.6.819.0..0i8i30k1j35i39k1.0.GfL6P3OclLE#imgrc=kNnzqmXdjHOL3M:.jpg" width="29" height="29" alt="">
+	<a href="default.asp">
+	  <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/07/Blue_%286100521867%29.jpg/1200px-Blue_%286100521867%29.jpg" alt="HTML tutorial" style="width:42px;height:42px;border:0;">
+	</a>
+
+</nav>
+
+
+
+
+<!DOCTYPE html>
+<html>
+<body>
+<form method="POST">
+
+enter unique user id:<input type="text" name="Roll">
+enter name:<input type="text" name="Name">
+<div class="form-group">
+  <div class="col-sm-offset-2 col-sm-10">
+    <button type="submit" class="btn btn-default">Submit</button>
+  <button type="reset" class="btn btn-default">Reset</button>
+  </div>
+</div>
+</form>
+
+<?php
+$server="localhost";
+$username="root";
+$pass="";
+$datatbase="exam1";
+$connect=mysqli_connect($server,$username,$pass,$datatbase);
+$n=$_POST["Roll"];
+$b=$_POST["Name"];
+$query="insert into student1(Name,Rollno) values('$b',$n)";
+$r=mysqli_query($connect,$query);
+
+if($r)
+{
+  echo "inserted";
+
+}
+else
+{
+  echo "error";
+}
+?>
+</body>
+</html>
+
+
+
+<blockquote class="blockquote text-center">
+  <p class="mb-0">When music hits you, you feel no pain.</p>
+  <footer class="blockquote-footer">Bob Marley <cite title="Source Title"></cite></footer>
+</blockquote>
+
+
+
+
+
+
+</body>
